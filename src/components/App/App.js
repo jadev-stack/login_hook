26279e10d6764613df9592c839ba5ff8b885ed1c
Juan <juanesx88@gmail.com>
2021-01-19 13:16:45 -0400

    Tutorial Login with Hook

---
+++ src/components/App/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Dashboard from "../Dashboard";
+import Preferences from "../Preferences";
+import Login from "../Login/Login";
+import useToken from "./useToken";
+
+function App() {
+  const { token, setToken } = useToken();
+
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/preferences">
+            <Preferences />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
